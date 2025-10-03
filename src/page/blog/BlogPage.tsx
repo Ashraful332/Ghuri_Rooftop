e01fc2c6a10d647e500cca8b56e1e09bef825ac5
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-03 17:02:09 +0600

    add blog details

--- src/page/blog/BlogPage.tsx
+++ src/page/blog/BlogPage.tsx
@@ -1,13 +1,91 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Link } from "react-router";
 import Footer from "../../components/navigation/Footer";
 import Navbar from "../../components/navigation/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const AdminUrl = import.meta.env.VITE_ADMIN_URL;
+
+type BlogData = {
+    _id: string,
+    title: string,
+    SortDes: string,
+    photoUrl: string,
+    description: string,
+    View: string
+
+}
 
 
 const BlogPage = () => {
+    const [blogs, setBlogs] = useState<BlogData[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // lode the blog
+    useEffect(() => {
+        async function LoadBlog() {
+            try {
+                const response = await axios.get(`${AdminUrl}/all-blog`)
+                setBlogs(response.data)
+                setIsLoading(false);
+            } catch (error) {
+                console.error("error is comming on get blog data")
+            }
+
+        }
+        LoadBlog()
+    }, [])
     return (
         <div>
-            <Navbar/>
-            <div></div>
-            <Footer/>
+            <Navbar />
+            <div className="mt-m mb-h">
+                <h3 className="text-center text-lg md:text-4xl font-bold mt-h mb-low ">All Blog</h3>
+                <div className="max-w-[1500px] mx-auto ">
+                    {isLoading ? (
+                        <div className="flex space-x-2 mt-7 mb-4">
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce"></div>
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.2s]"></div>
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.4s]"></div>
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.6s]"></div>
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.8s]"></div>
+                            <div className="w-4 h-4 bg-gray-500 rounded-full animate-bounce [animation-delay:-0.10s]"></div>
+                        </div>
+
+                    ) : (
+                        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 md:gap-6 md:p-4 mb-sec">
+                            {blogs.map((blog) => (
+                                <div key={blog._id} className=" w-[97vw] sm:w-[370px] ">
+                                    <div className="w-[97vw] sm:w-[350px] h-auto sm:h-[300px] overflow-hidden object-center relative ">
+                                        <img src={blog.photoUrl} alt="photo 1" className="w-full h-full object-cover rounded-lg " />
+                                    </div>
+                                    <div className="h-[180px] flex flex-col justify-between ">
+                                        <div>
+                                            <div className="mt-5 flex justify-between text-2xl">
+                                                <h4 className="font-bold text-sm">
+                                                    {blog.title}
+                                                </h4>
+                                            </div>
+                                            <p className="text-[var(--pTx-color)] text-sm pt-[6px] pr-2 pb-4  ">
+                                                {blog.SortDes}
+                                            </p>
+                                        </div>
+
+                                        <div className="flex justify-between">
+                                            <Link to={`/blog/${blog._id}`}
+                                                className="hero-btn ">
+                                                View Details
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
+            </div>
+            <Footer />
         </div>
     );
 };
---
+++ src/page/blog/details/BlogDetails.tsx
@@ -0,0 +1,111 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import CommentBlog from "./Comment";
+import  Eye  from "../../../assets/icon/menu.svg";
+import Navbar from "../../../components/navigation/Navbar";
+import Footer from "../../../components/navigation/Footer";
+
+
+const Public = import.meta.env.VITE_API_URL;
+
+type BlogData = {
+    _id: string,
+    title: string,
+    SortDes: string,
+    photoUrl: string,
+    description: string,
+    View: string
+
+}
+
+
+function wrapIframe(html: string): string {
+    // iframe ট্যাগ খুঁজে বের করে তার চারপাশে responsive div র‍্যাপ করো
+    return html.replace(
+        /<iframe.*?<\/iframe>/g,
+        (match) => `<div class="responsive-iframe">${match}</div>`
+    );
+}
+
+
+
+export default function BlogDetails() {
+    const [blogs, setBlogs] = useState<BlogData | any>(null)
+    const { id } = useParams(); // fecht the id of blog
+
+    // fetch the data
+    useEffect(() => {
+        async function LoadData() {
+            try {
+                const response = await axios.post(`${Public}/blog-details-all`, { id })
+                // const response = await axios.get(`${Public}/blog-details-all/${id}`)
+
+                setBlogs(response.data)
+                console.log(response.data);
+            } catch (error) {
+                console.error("the error is coming on load blog", error)
+            }
+        }
+        LoadData()
+    }, [id])
+
+    if (!blogs) {
+        return <p>Loading...</p>;
+    }
+
+    const safeHtml = wrapIframe(blogs.description);
+
+    return (
+        <div>
+            <Navbar />
+            <div className="flex flex-col items-center justify-center mx-auto mt-primary" >
+                <div className="mb-sec px-2 lg:p-0  ">
+                    <div>
+                        <img src={blogs.photoUrl} alt="Blog image" className="w-[100%] h-[500px] rounded-2xl object-cover " />
+                    </div>
+                    <div className="flex flex-col sm:flex-row items-center justify-between">
+                        <h2 className="text-lg sm:text-2xl text-white font-bold mt-2">{blogs.title}</h2>
+                        <div className="flex items-center gap-3">
+                            <img src={Eye} alt="" />
+                            <p>{blogs.View}</p>
+                        </div>
+                    </div>
+                    <p className="mt-3 w-full sm:w-[80%] text-sm sm:text-base ">{blogs.SortDes}</p>
+
+                    {/* Responsive iframe CSS */}
+                    <style>
+                        {`
+                        .responsive-iframe {
+                            position: relative;
+                            width: 100%;
+                            height: 0;
+                            padding-bottom: 56.25%;
+                            margin-top: 1rem;
+                            margin-bottom: 1rem;
+                        }
+                        .responsive-iframe iframe {
+                            position: absolute;
+                            width: 100%;
+                            height: 100%;
+                            left: 0;
+                            top: 0;
+                        }
+                        `}
+                    </style>
+                </div>
+
+                <div
+                    className="prose prose-lg mx-w-[90vw] lg:max-w-[70vw] mx-auto bg-[#0c0c0c7a] rounded-2xl p-6 "
+                    dangerouslySetInnerHTML={{ __html: safeHtml }}
+                />
+                {/* coment section */}
+                <div className="flex flex-col items-center mt-sec mb-sec">
+                    <CommentBlog MyBlogData={blogs} />
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
